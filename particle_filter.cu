527cae0284080d33ac78cf7386210ec8b2506d33
Er Jun Li <er.li@mail.mcgill.ca>
2021-11-29 01:35:13 -0500

    Parallel reduce and prefix sum to accelerate Particle Filter

--- particle_filter.cu
+++ particle_filter.cu
@@ -1,36 +1,49 @@
 #include <thrust/device_vector.h>
 #include <thrust/extrema.h>
 #include <thrust/host_vector.h>
+#include <thrust/scan.h>
 
+#include <chrono>
 #include <cmath>
 #include <iostream>
 #include <random>
 #include <vector>
-#include <chrono>
 
 #include "robot.cuh"
 
 using namespace std;
 
+// TODO fix bug where if block_size > 512 code fails
+
+__global__ void weight_normalization(double *weights, double weight_sum, const int N){
+  int index = threadIdx.x + blockDim.x * blockIdx.x;
+  if (index < N) {
+    weights[index] /= weight_sum;
+  }
+}
+
 /**
- * Simulate robot motion for each particle and perform importance weight
- * computation
+ * Simulates robot motion for each particle, performs measurement update and
+ * assigns a weight to each particle.
  */
 __global__ void particle_filter(Robot *particles, double *weights,
                                 const double theta, const double distance,
-                                const int N, const double *Z_gpu,
+                                const int N, const double *Z,
                                 const int num_landmarks,
                                 const double *landmarks_gpu) {
   int index = threadIdx.x + blockDim.x * blockIdx.x;
   if (index < N) {
     particles[index].move(theta, distance);
+
+    // likelihood of the real observation based on the current particle's state
     double prob = 1.0;
     for (int i = 0; i < num_landmarks; i++) {
-      double dist = sqrt((particles[index].get_x() - landmarks_gpu[i * 2 + 0]) *
-          (particles[index].get_x() - landmarks_gpu[i * 2 + 0]) +
-          (particles[index].get_y() - landmarks_gpu[i * 2 + 1]) *
-              (particles[index].get_y() - landmarks_gpu[i * 2 + 1]));
-      prob *= Gaussian(dist, particles[index].get_sense_noise(), Z_gpu[i]);
+      double dist =
+          sqrt((particles[index].get_x() - landmarks_gpu[i * 2 + 0]) *
+                   (particles[index].get_x() - landmarks_gpu[i * 2 + 0]) +
+               (particles[index].get_y() - landmarks_gpu[i * 2 + 1]) *
+                   (particles[index].get_y() - landmarks_gpu[i * 2 + 1]));
+      prob *= Gaussian(dist, particles[index].get_sense_noise(), Z[i]);
     }
     weights[index] = prob;
   }
@@ -38,6 +51,7 @@ __global__ void particle_filter(Robot *particles, double *weights,
 int main(int argc, char *argv[]) {
   // physical robot (ground truth)
   auto start = chrono::high_resolution_clock::now();
+
   if (argc != 4) {
     cout << "Usage: ./particle_filter_gpu <number of particles> <number of "
             "iterations of particle filtering> <number of threads per block>"
@@ -45,12 +59,13 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
+  // physical robot (ground truth)
   Robot my_robot;
 
   // number of particles (TODO: set default to 1000)
   int N = atoi(argv[1]);
 
-  // number of iterations of particle filter (TODO: set default to 10)
+  // number of particle filter iterations (TODO: set default to 10)
   int T = atoi(argv[2]);
 
   int block_size = atoi(argv[3]);
@@ -61,8 +76,8 @@ int main(int argc, char *argv[]) {
   size_t landmark_size = sizeof(double) * num_landmarks * landmark_dim;
 
   // initialize N random particles (robots)
-  // list of particles  (guesses as to where the robot might be - each particle
-  // is a vector representing the state of the robot (x,y,theta)  theta is the
+  // list of particles (guesses as to where the robot might be - each particle
+  // is a vector representing the state of the robot (x,y,theta) theta is the
   // angle relative to the x-axis)
   vector<Robot> particles(N);
   Robot *particles_gpu;
@@ -70,11 +85,9 @@ int main(int argc, char *argv[]) {
   thrust::host_vector<double> weights(N);
   double *landmarks_gpu;
   cudaMalloc(&landmarks_gpu, landmark_size);
-  cudaMemcpy(landmarks_gpu, landmarks, landmark_size, cudaMemcpyHostToDevice);
-
-  // copy those particles on the GPU
   cudaMalloc(&particles_gpu, particles_size);
-  // cudaMallocManaged(&weights, N * sizeof(double));
+  
+  cudaMemcpy(landmarks_gpu, landmarks, landmark_size, cudaMemcpyHostToDevice);
   cudaMemcpy(particles_gpu, particles.data(), particles_size,
              cudaMemcpyHostToDevice);
 
@@ -82,6 +95,9 @@ int main(int argc, char *argv[]) {
     double theta = uniform_distribution_sample() * M_PI / 2;
     double distance = (uniform_distribution_sample() * 9.0) + 1;
     my_robot.move(theta, distance);
+
+    // detect distance of the real robot to the landmarks in our world (returns
+    // list of distance to each obstacle)
     vector<double> Z = my_robot.sense();
     thrust::device_vector<double> Z_gpu(Z);
 
@@ -91,36 +107,52 @@ int main(int argc, char *argv[]) {
         landmarks_gpu);
     cudaDeviceSynchronize();
 
-    thrust::copy(weights_gpu.begin(), weights_gpu.end(), weights.begin());
-    double max_w = *(thrust::max_element(weights.begin(), weights.end()));
+    // weights summation via parallelization
+    double weights_sum = thrust::reduce(weights_gpu.begin(), weights_gpu.end()); // , thrust::plus<double>()
 
+    weight_normalization<<<num_block, block_size>>>(thrust::raw_pointer_cast(weights_gpu.data()), weights_sum, N);
+
+    // copying normalized particles' weights from GPU to CPU
+    thrust::copy(weights_gpu.begin(), weights_gpu.end(), weights.begin());
+
+    // copying updated particles to CPU
     cudaMemcpy(particles.data(), particles_gpu, particles_size,
                cudaMemcpyDeviceToHost);
 
-    // resampling
+    // compute cumulative distribution function (CDF)
+    thrust::device_vector<double> cdf_gpu(weights_gpu.size());
+    thrust::inclusive_scan(weights_gpu.begin(), weights_gpu.end(), cdf_gpu.begin());
+    thrust::host_vector<double> cdf(cdf_gpu);
+
+    int k = 0;
+    auto u = [&N](int n) {
+      return (((n - 1) + uniform_distribution_sample()) / N);
+    };
+
+    // systematic resampling of new particles
     vector<Robot> new_particles;
-    int index = (int) (uniform_distribution_sample() * N);
-    double beta = 0.0;
-    for (int m = 0; m < N; m++) {
-      beta += uniform_distribution_sample() * 2.0 * max_w;
-      while (beta > weights[index]) {
-        beta -= weights[index];
-        index = (index + 1) % N;
+    for (int i = 1; i <= N; i++) {
+      while (cdf[k] < u(i)) {
+        k += 1;
       }
-      new_particles.push_back(particles[index]);
+      new_particles.push_back(particles[k]);
     }
     particles = new_particles;
+
+    // copy new particles to GPU for next iteration
     cudaMemcpy(particles_gpu, particles.data(), particles_size,
                cudaMemcpyHostToDevice);
 
-//    cout << eval(my_robot, particles) << endl;
+    cout << eval(my_robot, particles) << endl;
   }
 
   cudaFree(particles_gpu);
   cudaFree(landmarks_gpu);
   auto finish = chrono::high_resolution_clock::now();
-  std::cout << block_size << "," << N << "," << chrono::duration_cast<chrono::nanoseconds>(finish - start).count()
-            << "\n";
+  std::cout
+      << block_size << "," << N << ","
+      << chrono::duration_cast<chrono::nanoseconds>(finish - start).count()
+      << "\n";
 
   return 0;
 }
