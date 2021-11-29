4482b3093ebb2e9931e3140e357e165c8c8e68bb
ding ma <ding.ma@mail.mcgill.ca>
2021-11-29 01:46:11 -0500

    profiled with new way

--- Profiling/summarize.py
+++ Profiling/summarize.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-# df = pd.read_csv("parallel_manual.csv")
-# df = df.groupby(['block_size','num_particles']).mean().reset_index()
-# df.to_csv("parallel_manual_summary.csv", index=False)
+df = pd.read_csv("parallel_manual.csv")
+df = df.groupby(['block_size','num_particles']).mean().reset_index()
+df.to_csv("parallel_manual_summary.csv", index=False)
 
 df = pd.read_csv("sequential.csv")
 df = df.groupby(['num_particles']).mean().reset_index()
