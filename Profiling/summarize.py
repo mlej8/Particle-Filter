a908e15ce0520bebe9484807262af1dfc3e9ff93
ding ma <ding.ma@mail.mcgill.ca>
2021-11-29 00:39:42 -0500

    systemic sampling

--- Profiling/summarize.py
+++ Profiling/summarize.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv("parallel_manual.csv")
-df = df.groupby(['block_size','num_particles']).mean().reset_index()
-df.to_csv("parallel_manual_summary.csv", index=False)
+# df = pd.read_csv("parallel_manual.csv")
+# df = df.groupby(['block_size','num_particles']).mean().reset_index()
+# df.to_csv("parallel_manual_summary.csv", index=False)
 
 df = pd.read_csv("sequential.csv")
-df = df.groupby(['number_particles']).mean().reset_index()
+df = df.groupby(['num_particles']).mean().reset_index()
 df.to_csv("sequential_summary.csv", index=False)
